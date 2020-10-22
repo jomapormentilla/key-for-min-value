6c4e5405bb04763ee23ef9a7a6d7ec7634a43c9a
jomapormentilla <jomapormentilla@gmail.com>
2020-10-22 16:52:48 -0400

    commit tree b1c6701de4c53b6b4849885368defc7dafbfbfc1

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,18 @@
 
 def key_for_min_value(name_hash)
 
+    compare = 0
+    result = nil
+    name_hash.each do |key, value|
+        if compare == 0
+            compare = value
+            result = key
+        else
+            if value <= compare
+                result = key
+            end
+        end
+    end
+
+    result
 end
